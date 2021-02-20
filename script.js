a04f0943edd84fa211afc1a5b211ffe1f9c7c344
Leth4L <carribean_08@mail.ru>
2021-02-20 10:27:47 +0300

    feat: add smooth scroll, burger menu, slider on js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(function () {
 
+    // Header
     let header = $('#header');
     let intro = $('#intro');
     let introHeight = intro.innerHeight();
@@ -15,4 +16,41 @@ $(function () {
             header.removeClass('fixed');
         }
     });
+
+    // Smooth scroll
+    $('[data-scroll]').on('click', function(e) {
+        e.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass('show');
+
+        $('html, body').animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+
+    // Burger menu
+    let nav = $('#nav');
+
+    $('#navToggle').on('click', function(e) {
+        e.preventDefault();
+
+        nav.toggleClass('show');
+    });
+
+    // reviewsSlider
+    let slider = $('#reviewsSlider');
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
 });
